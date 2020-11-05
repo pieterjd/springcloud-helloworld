6bfcca0ca2ead48b53cb40282412a0a3974048cb
Pieter-Jan Drouillon <drouillon@hotmail.com>
2020-11-05 19:53:05 +0100

    Cluster internet connected nodes

--- diagrams/diagram.py
+++ diagrams/diagram.py
@@ -1,18 +1,18 @@
 # use diagrams from https://github.com/mingrammer/diagrams
-from diagrams import Diagram
+from diagrams import Diagram, Cluster
 from diagrams.programming.framework import Spring
 from diagrams.onprem.client import Client
+
 with Diagram("springcloud-helloworld"):
+    with Cluster("Internet"):
+        client = Client("Client")
+        greeting = Spring("Greeting Service")
     eureka = Spring("Eureka Server")
-    greeting = Spring("Greeting Service")
     salutation = Spring("Salutation Service")
     datetime = Spring("Datetime Service")
-    client = Client("Client")
     client >> greeting
     greeting >> eureka
     greeting >> salutation
     greeting >> datetime
     salutation >> eureka
     datetime >> eureka
-
-
